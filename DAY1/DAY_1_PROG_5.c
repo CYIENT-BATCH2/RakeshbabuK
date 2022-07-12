c2db09b7f1ee54b0183787adf2cd2b51abb3c868
RakeshbabuK <108973932+RakeshbabuK@users.noreply.github.com>
2022-07-12 23:13:09 +0530

    Create DAY_1_PROG_5.c

---
+++ DAY1/DAY_1_PROG_5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a=1,b=2,c=3,d=4,e=5;
+    if(a%2==0)
+    printf("a is even\n");
+    else
+    printf("a is odd\n");
+    if(b%2==0)
+    printf("b is even\n");
+    else
+    printf("b is odd\n");
+    if(c%2==0)
+    printf("c is even\n");
+    else
+    printf("c is odd\n");
+    if(d%2==0)
+    printf("d is even\n");
+    else
+    printf("d is odd\n");
+    if(e%2==0)
+    printf("e is even\n");
+    else
+    printf("e is odd\n");
+}
