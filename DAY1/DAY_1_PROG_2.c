058f1c56baaeee851b082adb573bc5357be9f39b
RakeshbabuK <108973932+RakeshbabuK@users.noreply.github.com>
2022-07-12 22:54:02 +0530

    Create DAY_1_PROG_2.c

---
+++ DAY1/DAY_1_PROG_2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("enter the character:");
+    scanf("%c",&ch);
+    ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'?printf("it is an vowel"):printf("it is a consonent");
+}
