4378f961ee4dc447278be380412bd5ed900c9811
RakeshbabuK <108973932+RakeshbabuK@users.noreply.github.com>
2022-07-13 00:08:08 +0530

    Create DAY_3_PROG_2.c

---
+++ DAY3/DAY_3_PROG_2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int num,rem,cnt=0,sum=0;
+    printf("enter the 5 digit number:");
+    scanf("%d",&num);
+    if(num>9999&&num<=99999)
+    {
+LOOP:   rem=num%10;
+        cnt++;
+        if(cnt==2||cnt==5)
+        sum=sum+rem;
+        num=num/10;
+        if(num>0)
+        goto LOOP;
+        printf("sum=%d\n",sum);
+    }
+    else
+    {
+        printf("the given number is not a 5 digit:");    
+    }
+}
