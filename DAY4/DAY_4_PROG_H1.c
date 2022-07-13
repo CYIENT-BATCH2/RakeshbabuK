da60e65b0bfd5a39342e62a8cab4a07a5bbb3d65
RakeshbabuK <108973932+RakeshbabuK@users.noreply.github.com>
2022-07-13 22:19:32 +0530

    Create DAY_4_PROG_H1.c

---
+++ DAY4/DAY_4_PROG_H1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the values of a and b:");
+    scanf("%d %d",&a,&b);
+    a>b?printf("a is the biggest number\n"):printf("b is the biggest number\n");
+}
